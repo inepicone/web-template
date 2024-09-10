10ea22a979609d35d9a5edd47ec242847e579ad0
Patricio <patricio_cristaldo@outlook.com>
2024-09-10 08:53:07 -0300

    revierto cambios en initiate-pivileged

--- server/api/initiate-privileged.js
+++ server/api/initiate-privileged.js
@@ -6,51 +6,21 @@ const {
   serialize,
   fetchCommission,
 } = require('../api-util/sdk');
-
-// Definir comisiones para tiendas y particulares
-const COMISION_TIENDAS = 0.10; // 10%
-const COMISION_PARTICULARES = 0.20; // 20%
-
-// Función para obtener la comisión según el tipo de usuario
-const calcularComision = (tipoUsuario) => {
-  if (tipoUsuario === 'tienda') {
-    return COMISION_TIENDAS;
-  } else if (tipoUsuario === 'particular') {
-    return COMISION_PARTICULARES;
-  } else {
-    throw new Error("Tipo de usuario no reconocido");
-  }
-};
-
 module.exports = (req, res) => {
   const { isSpeculative, orderData, bodyParams, queryParams } = req.body;
   const sdk = getSdk(req, res);
   let lineItems = null;
-
-  // Obtener los detalles del anuncio (listing) y la comisión
   const listingPromise = () => sdk.listings.show({ id: bodyParams?.params?.listingId });
-
   Promise.all([listingPromise(), fetchCommission(sdk)])
     .then(([showListingResponse, fetchAssetsResponse]) => {
       const listing = showListingResponse.data.data;
       const commissionAsset = fetchAssetsResponse.data.data[0];
-
-      // Obtener la comisión del proveedor (si existe en el asset de la API)
       const { providerCommission, customerCommission } =
         commissionAsset?.type === 'jsonAsset' ? commissionAsset.attributes.data : {};
-
-      // Obtener el tipo de usuario del proveedor desde el listing
-      const providerType = listing?.relationships?.author?.data?.attributes?.profile?.type;  // Asegúrate que este sea el campo correcto
-      const comisionCalculada = calcularComision(providerType);
-
-      // Combinar la comisión calculada con los valores obtenidos de fetchCommission
-      const comisionProveedorFinal = providerCommission || comisionCalculada;
-
-      // Generar las líneas de pedido incluyendo la comisión
       lineItems = transactionLineItems(
         listing,
         { ...orderData, ...bodyParams.params },
-        comisionProveedorFinal,
+        providerCommission,
         customerCommission
       );
 
@@ -58,8 +28,7 @@ module.exports = (req, res) => {
     })
     .then(trustedSdk => {
       const { params } = bodyParams;
-
-      // Añadir lineItems a los body params
+      // Add lineItems to the body params
       const body = {
         ...bodyParams,
         params: {
@@ -67,8 +36,6 @@ module.exports = (req, res) => {
           lineItems,
         },
       };
-
-      // Iniciar transacción especulativa o no especulativa según corresponda
       if (isSpeculative) {
         return trustedSdk.transactions.initiateSpeculative(body, queryParams);
       }
