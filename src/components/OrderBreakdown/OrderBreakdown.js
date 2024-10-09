460262673dfd9c80fc4f340bf190e09b0e1c0eb4
Patricio <patricio_cristaldo@outlook.com>
2024-10-09 08:31:15 -0300

    Queda funcionando el desglose con descuento aplicado para los casos tienda (10%)  y persona (20%)

--- src/components/OrderBreakdown/OrderBreakdown.js
+++ src/components/OrderBreakdown/OrderBreakdown.js
@@ -2,15 +2,9 @@ import React from 'react';
 import { oneOf, string } from 'prop-types';
 import classNames from 'classnames';
 import { FormattedMessage, intlShape, injectIntl } from '../../util/reactIntl';
-import {
-  propTypes,
-  LISTING_UNIT_TYPES,
-  LINE_ITEM_CUSTOMER_COMMISSION,
-  LINE_ITEM_PROVIDER_COMMISSION,
-} from '../../util/types';
+import { propTypes, LISTING_UNIT_TYPES, LINE_ITEM_CUSTOMER_COMMISSION } from '../../util/types';
 import LineItemBookingPeriod from './LineItemBookingPeriod';
 import LineItemBasePriceMaybe from './LineItemBasePriceMaybe';
-import LineItemSubTotalMaybe from './LineItemSubTotalMaybe';
 import LineItemShippingFeeMaybe from './LineItemShippingFeeMaybe';
 import LineItemPickupFeeMaybe from './LineItemPickupFeeMaybe';
 import LineItemCustomerCommissionMaybe from './LineItemCustomerCommissionMaybe';
@@ -39,39 +33,58 @@ export const OrderBreakdownComponent = props => {
   const unitLineItem = lineItems?.find(
     item => LISTING_UNIT_TYPES.includes(item.code) && !item.reversal
   );
-
   const lineItemUnitType = unitLineItem?.code;
-
-  const hasCommissionLineItem = lineItems.find(item => {
-    const hasCustomerCommission = isCustomer && item.code === LINE_ITEM_CUSTOMER_COMMISSION;
-    const hasProviderCommission = isProvider && item.code === LINE_ITEM_PROVIDER_COMMISSION;
-    return (hasCustomerCommission || hasProviderCommission) && !item.reversal;
-  });
 
   // Calcular el subtotal sumando los montos de todos los line items
   const subtotal = lineItems.reduce((sum, item) => {
     return sum + (item.lineTotal.amount || 0);
   }, 0);
 
+  // Formatear el subtotal
+  const formattedSubTotal = intl.formatNumber(subtotal / 100, {
+    style: 'currency',
+    currency,
+  });
+
   // Extraer la comisión del cliente
-  const customerCommissionItems = lineItems.filter(item => 
-    item.code === LINE_ITEM_CUSTOMER_COMMISSION && !item.reversal
+  const customerCommissionItems = lineItems.filter(
+    item => item.code === LINE_ITEM_CUSTOMER_COMMISSION && !item.reversal
   );
   const customerCommissionTotal = customerCommissionItems.reduce((sum, item) => {
     return sum + (item.lineTotal.amount || 0);
   }, 0);
 
-  // Restar la comisión del cliente al subtotal
-  const finalTotalValue = subtotal - customerCommissionTotal;
+  // Crear una nueva variable con la suma de los valores numéricos de customerCommissionTotal y subtotal
+  const totalSumValue = subtotal + customerCommissionTotal;
 
-  // Formatear el resultado final
-  const formattedFinalTotal = intl.formatNumber(finalTotalValue / 100, {
+  // Formatear el resultado total
+  const formattedTotalSum = intl.formatNumber(totalSumValue / 100, {
     style: 'currency',
     currency,
   });
 
-  const classes = classNames(rootClassName || css.root, className);
+  // Calcular si la comisión del cliente es el 10% del totalSumValue
+  const isCustomerCommission10Percent = customerCommissionTotal === totalSumValue * 0.1;
 
+  // Ajustar el valor final dependiendo de si la comisión es el 10%
+  const adjustedFinalTotal = isCustomerCommission10Percent
+    ? intl.formatNumber(((subtotal - customerCommissionTotal) * 0.9) / 100, {
+        style: 'currency',
+        currency,
+      })
+    : intl.formatNumber(subtotal / 100, {
+        style: 'currency',
+        currency,
+      });
+
+  const classes = classNames(rootClassName || css.root, className);
+  const newSubtotal = subtotal - customerCommissionTotal;
+  const formattedNewSubtotal = intl.formatNumber(newSubtotal / 100, {
+    style: 'currency',
+    currency,
+  }); 
+  //Cuando customerCommissionTotal(Monto de reserva) es el 20% del subtotal(A pagar al oferente) 
+  //muestra formattedNewSubtotal(subtotal - customerCommissionTotal) en A pagar al oferente (caso persona) sino muestra formattedSubTotal(caso tienda)
   return (
     <div className={classes}>
       <LineItemBookingPeriod
@@ -85,13 +98,16 @@ export const OrderBreakdownComponent = props => {
       <LineItemPickupFeeMaybe lineItems={lineItems} intl={intl} />
       <LineItemUnknownItemsMaybe lineItems={lineItems} isProvider={isProvider} intl={intl} />
 
-      {/* Mostrar el subtotal/A pagar al oferente */}
       <div className={css.subTotalLineItem}>
         <span className={css.itemLabel}>
           <FormattedMessage id="OrderBreakdown.subTotal" />
         </span>
         <span className={css.itemValue}>
-          {formattedFinalTotal} 
+          {customerCommissionTotal === subtotal * 0.8
+            ? adjustedFinalTotal
+            : customerCommissionTotal === subtotal * 0.2
+            ? formattedNewSubtotal
+            : formattedSubTotal}
         </span>
       </div>
 
@@ -103,20 +119,27 @@ export const OrderBreakdownComponent = props => {
       />
       <LineItemProviderCommissionMaybe
         lineItems={lineItems}
-        isProvider={isProvider}
+        isProvider={userRole === 'provider'}
         marketplaceName={marketplaceName}
         intl={intl}
       />
 
-      <LineItemTotalPrice transaction={transaction} isProvider={isProvider} intl={intl} />
-
-      {hasCommissionLineItem && (
-        <span className={css.feeInfo}>
-          <FormattedMessage id="OrderBreakdown.commissionFeeNote" />
-          <br />
-          <FormattedMessage id="OrderBreakdown.commissionFeeNote2" />
-        </span>
-      )}
+      <div className={css.commissionLineItem}>
+        <hr className={css.totalDivider} />
+        <div className={css.lineItemTotal}>
+          <div className={css.totalLabel}>
+            <FormattedMessage id="OrderBreakdown.total" />
+          </div>
+          <div className={css.totalPrice}>
+            {customerCommissionTotal === subtotal * 0.2 ? adjustedFinalTotal : formattedTotalSum} 
+          </div>
+        </div>
+      </div>
+      <span className={css.feeInfo}>
+        <FormattedMessage id="OrderBreakdown.commissionFeeNote" />
+        <br />
+        <FormattedMessage id="OrderBreakdown.commissionFeeNote2" />
+      </span>
     </div>
   );
 };
