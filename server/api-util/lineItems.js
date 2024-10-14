a99fae63be0eaa3bbb67f3d683e3fa96329f0c0e
Patricio <patricio_cristaldo@outlook.com>
2024-10-14 08:31:55 -0300

    Revierto cambios en lineItems

--- server/api-util/lineItems.js
+++ server/api-util/lineItems.js
@@ -103,7 +103,6 @@ const getDateRangeUnitsSeatsLineItems = (orderData, code) => {
   return { units, seats, extraLineItems: [] };
 };
 /**
- * Returns collection of lineItems (max 50)
  *
  * @param {Object} listing
  * @param {Object} orderData
@@ -115,35 +114,12 @@ exports.transactionLineItems = (listing, orderData, providerCommission, customer
   const publicData = listing.attributes.publicData;
   const unitPrice = listing.attributes.price;
   const currency = unitPrice.currency;
-  const { obtenerComisionCliente } = require('./lineItemHelpers'); // Ajusta la ruta del archivo
-  const comisionCliente = obtenerComisionCliente(publicData);
-  const userType = orderData.userType;
-
-  /**
-   * Lógica para determinar el porcentaje de comisión del cliente basado en userType
-   */
-  let customerCommissionPercentage = comisionCliente;
-
-  if (userType === 'tienda') {
-    customerCommissionPercentage = 10; // 10% para "tienda"
-  } else {
-    customerCommissionPercentage = 10; // 20% para "persona_fisica"
-  } 
-
-  // Actualizar el objeto customerCommission con el porcentaje determinado
-  const adjustedCustomerCommission = {
-    ...customerCommission,
-    percentage: customerCommissionPercentage,
-  };
-
-  /**
-   * Función auxiliar para resolver el precio de la tarifa de casco (helmet fee)
-   */
-  const resolveHelmetFeePrice = (listing) => {
+ 
+  const resolveHelmetFeePrice = listing => {
     const publicData = listing.attributes.publicData;
     const helmetFee = publicData && publicData.helmetFee;
-    const { amount, currency } = helmetFee || {};
-
+    const { amount, currency } = helmetFee;
+  
     if (amount && currency) {
       return new Money(amount, currency);
     }
@@ -152,11 +128,23 @@ exports.transactionLineItems = (listing, orderData, providerCommission, customer
   };
   
   /**
-   * Cálculo de la cantidad y los line-items extra según el tipo de unidad
+   * Pricing starts with order's base price:
+   * Listing's price is related to a single unit. It needs to be multiplied by quantity
+   *
+   * Initial line-item needs therefore:
+   * - code (based on unitType)
+   * - unitPrice
+   * - quantity
+   * - includedFor
    */
+
+  // Unit type needs to be one of the following:
+  // day, night, hour or item
   const unitType = publicData.unitType;
   const code = `line-item/${unitType}`;
 
+  // Here "extra line-items" means line-items that are tied to unit type
+  // E.g. by default, "shipping-fee" is tied to 'item' aka buying products.
   const quantityAndExtraLineItems =
     unitType === 'item'
       ? getItemQuantityAndLineItems(orderData, publicData, currency)
@@ -167,10 +155,8 @@ exports.transactionLineItems = (listing, orderData, providerCommission, customer
       : ['day', 'night'].includes(unitType)
       ? getDateRangeQuantityAndLineItems(orderData, code)
       : {};
-
   const { quantity, units, seats, extraLineItems } = quantityAndExtraLineItems;
-
-  // Validación de la información de cantidad
+  // Throw error if there is no quantity information given
   if (!quantity && !(units && seats)) {
     const message = `Error: transition should contain quantity information: 
       stockReservationQuantity, quantity, units & seats, or bookingStart & bookingEnd (if "line-item/day" or "line-item/night" is used)`;
@@ -180,39 +166,49 @@ exports.transactionLineItems = (listing, orderData, providerCommission, customer
     error.data = {};
     throw error;
   }
-
-  // Determinar si se usa cantidad o unidades y asientos
+  // A booking line item can have either quantity, or units and seats. Add the
+  // correct values depending on whether units and seats exist.
   const quantityOrSeats = !!units && !!seats ? { units, seats } : { quantity };
-
   /**
-   * Crear el line-item base para la orden
-   */
+   * If you want to use pre-defined component and translations for printing the lineItems base price for order,
+   * you should use one of the codes:
+   * line-item/night, line-item/day, line-item/hour or line-item/item.
+   *
+   * Pre-definded commission components expects line item code to be one of the following:
+   * 'line-item/provider-commission', 'line-item/customer-commission'
+   *
+   * By default OrderBreakdown prints line items inside LineItemUnknownItemsMaybe if the lineItem code is not recognized. */
+
   const order = {
     code,
     unitPrice,
     ...quantityOrSeats,
     includeFor: ['customer', 'provider'],
   };
-
-  // Resolver la tarifa de casco si aplica
-  const helmetFeePrice = orderData.hasHelmetFee ? resolveHelmetFeePrice(listing) : null;
-  const helmetFee = helmetFeePrice
-    ? [
-        {
-          code: 'line-item/Descuento por días de alquiler',
-          unitPrice: helmetFeePrice,
-          quantity: -1,
-          includeFor: ['customer', 'provider'],
-        },
-      ]
-    : [];
-
-  // Función para obtener la negación del porcentaje (para la comisión del proveedor)
-  const getNegation = (percentage) => {
+   const helmetFeePrice = orderData.hasHelmetFee ? resolveHelmetFeePrice(listing) : null;
+   const helmetFee = helmetFeePrice
+     ? [
+         {
+           code: 'line-item/Descuento por días de alquiler',
+           unitPrice: helmetFeePrice,
+           quantity: -1,
+           includeFor: ['customer', 'provider'],
+         },
+       ]
+     : [];
+     
+  // Provider commission reduces the amount of money that is paid out to provider.
+  // Therefore, the provider commission line-item should have negative effect to the payout total.
+  const getNegation = percentage => {
     return -1 * percentage;
   };
 
-  // Comisiones del proveedor
+  // Note: extraLineItems for product selling (aka shipping fee)
+  // is not included in either customer or provider commission calculation.
+
+  // The provider commission is what the provider pays for the transaction, and
+  // it is the subtracted from the order price to get the provider payout:
+  // orderPrice - providerCommission = providerPayout
   const providerCommissionMaybe = hasCommissionPercentage(providerCommission)
     ? [
         {
@@ -224,14 +220,16 @@ exports.transactionLineItems = (listing, orderData, providerCommission, customer
       ]
     : [];
 
-  // **Comisiones del cliente ajustadas según userType**
-  const customerCommissionMaybe = hasCommissionPercentage(adjustedCustomerCommission)
-    ? [
-        {
-          code: 'line-item/customer-commission',
-          unitPrice: calculateTotalFromLineItems([order, ...helmetFee]),
-          percentage: adjustedCustomerCommission.percentage,
-          includeFor: ['customer'],
+  // The customer commission is what the customer pays for the transaction, and
+  // it is added on top of the order price to get the customer's payin price:
+  // orderPrice + customerCommission = customerPayin
+  const customerCommissionMaybe = hasCommissionPercentage(customerCommission)
+  ? [
+      {
+        code: 'line-item/customer-commission',
+        unitPrice: calculateTotalFromLineItems([order, ...helmetFee]),
+        percentage:customerCommission.percentage,
+        includeFor: ['customer'],
         },
       ]
     : [];
